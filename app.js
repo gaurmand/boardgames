07806da1753194acbaa475b5221b2abafbbfff66
gaurmand <gaurav_amwan@hotmail.com>
2019-12-27 18:03:50 -0800

    Send index html file, add console log

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    console.log('Received request')
+    res.sendFile('/public/app/index.html')
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
