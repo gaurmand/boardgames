bc61fbd4ef3f15141f931fc08fb0ccb651731025
gaurmand <gaurav_amwan@hotmail.com>
2019-12-28 19:10:41 -0800

    Move logging middleware to app.js

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+// middleware that logs request
+app.use((req, res, next) => {
+  console.log(`[LOG] ${new Date().toLocaleTimeString()}: Received ${req.method} request for ${req.hostname}/${req.originalUrl}: `)
+  next()
+})
+
 const boardgames = require('./boardgames')
 app.use('/boardgames', boardgames)
+
+app.get('/', (req, res) => res.send('Requewt for root')
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
--- boardgames.js
+++ boardgames.js
@@ -1,13 +1,6 @@
 const express = require('express')
 const boardgames = express.Router()
 
-// middleware that is specific to this router
-boardgames.use((req, res, next) => {
-  console.log(`${new Date().toLocaleTimeString()}: Received ${req.method} request for ${req.originalUrl}: `)
-  next()
-})
-
-// define the home page route
 boardgames.get('/',(req, res) => {
   res.sendFile(__dirname+'/public/app/index.html')
 })
