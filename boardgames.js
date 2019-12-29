2ea93d2a013ad4cfb0722c41aefe9175df7b23c4
gaurmand <gaurav_amwan@hotmail.com>
2019-12-29 13:05:10 -0800

    Fix repeated variable name res

--- boardgames.js
+++ boardgames.js
@@ -7,12 +7,12 @@ const client = new Client()
 client.connect()
 
 boardgames.get('/',(req, res, next) => {
-    client.query('SELECT * FROM player_stat;', (err, res) => {
+    client.query('SELECT * FROM player_stat;', (err, result) => {
       client.end()
       if (err)
           next(err)
       else{
-        console.log(res.rows)
+        console.log(result.rows)
         res.render('index', { title: "Leo's Board Game Corner", message: 'coming soon' })
       }
     })  
