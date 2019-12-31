4de5cb7dec8b0ff23b9d4d5d30768a7a81055a98
gaurmand <gaurav_amwan@hotmail.com>
2019-12-30 20:37:13 -0800

    Test updating player_stat records in /games/new route

--- boardgames.js
+++ boardgames.js
@@ -56,7 +56,36 @@ boardgames.get('/games/new',(req, res) => {
 })
 boardgames.post('/games/new',(req, res) => {
   console.log(req.body)
-  insertRecord(res, 'INSERT INTO game(name, description) VALUES($1, $2)', [req.body.name, req.body.desc])
+  
+  let name = req.body.name
+  let desc = req.body.desc
+
+  beginTransaction(() => {
+    client.query('INSERT INTO game(name, description) VALUES($1, $2) RETURNING game_id', [name, desc], (err, result) => {
+      if (err){
+        console.error(err)
+        res.send('Error on insert')
+        rollbackTransaction()
+      } else{
+        client.query('SELECT * FROM player;', (err, result) => {
+          if (err){
+            console.error(err)
+            res.send('Error on select')
+            rollbackTransaction()
+          } else{
+            let players = result.rows;
+            let query_text = 'INSERT INTO player_stat(game_id, player_id, num_wins, num_losses, num_draws, elo) VALUES'
+            players.forEach((player, i) => {query_text+=`($${2*i+1},$${2*i+2},0,0,0,0)` + (i==players.length-1 ? ';':',')})
+            
+            console.log('query text: '+query_text)
+            console.log('game_id: '+result)
+            let values = []
+          }
+        }
+      }
+    }
+  })
+
 })
 
 function getWinPercentage(wins, losses, draws){
@@ -64,13 +93,44 @@ function getWinPercentage(wins, losses, draws){
   return num_games ? (100*wins/num_games).toFixed(2)+'%' : "0%"
 }
 
-function insertRecord(res, queryText, params){
-  client.query(queryText, params, (err, result) => {
+function insertRecord(res, queryText, values){
+  client.query(queryText, values, (err, result) => {
     if (err){
       console.error(err)
       res.send('Error on insert')
     } else
       res.redirect('/')
+  })
+}
+
+function performQuery(query_text, values, error, success){
+
+}
+
+function beginTransaction(next){
+  client.query('BEGIN', err => {
+    if (err)
+      console.error(err)
+    else
+      next()
+  })
+}
+
+function rollbackTransaction(next){
+  client.query('ROLLBACK', err => {
+    if (err)
+      console.error(err)
+    else if (next)
+      next()
+  })
+}
+
+function commitTransaction(next){
+  client.query('COMMIT', err => {
+    if (err)
+      console.error(err)
+    else if (next)
+      next()
   })
 }
 
